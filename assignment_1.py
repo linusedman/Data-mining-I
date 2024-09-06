cd9582b9c7f75b0ca317fe5f7d24703a096e870d
linusedman <linus.edman33@gmail.com>
2024-09-06 11:10:27 +0200

    Reading files

--- assignment_1.py
+++ assignment_1.py
@@ -1,1 +1,16 @@
 import pandas as pd
+import numpy as np
+
+data = pd.read_csv("Data-mining-I/iris_data.csv")
+labels = pd.read_csv("Data-mining-I/iris_labels-1.csv")
+
+# data = pd.read_csv("iris_data.csv")
+# labels = pd.read_csv("iris_labels-1.csv")
+
+print("Shape of data: ", data.shape)
+print("Head of data", data.head)
+
+print("Shape of labels: ", labels.shape)
+print("Head of labels: ", labels.head)
+
+dataframe = pd.merge(data, labels, on = 'id', how = "inner")
