bccb6d01f4f1f5e52a9cb6492e534c48ffefc176
olma5249 <olma5249@student.uu.se>
2024-09-06 11:16:54 +0200

    l

--- assignment_1.py
+++ assignment_1.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("Data-mining-I/iris_data.csv")
-labels = pd.read_csv("Data-mining-I/iris_labels-1.csv")
+#data = pd.read_csv("Data-mining-I/iris_data.csv")
+#labels = pd.read_csv("Data-mining-I/iris_labels-1.csv")
 
-# data = pd.read_csv("iris_data.csv")
-# labels = pd.read_csv("iris_labels-1.csv")
+data = pd.read_csv("iris_data.csv")
+labels = pd.read_csv("iris_labels-1.csv")
 
 print("Shape of data: ", data.shape)
 print("Head of data", data.head)
