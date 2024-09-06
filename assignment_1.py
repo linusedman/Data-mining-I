eafc8c772e1c542437b3cb622d2f24fe551aff41
linusedman <linus.edman33@gmail.com>
2024-09-06 11:19:59 +0200

    i works!

--- assignment_1.py
+++ assignment_1.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-#data = pd.read_csv("Data-mining-I/iris_data.csv")
-#labels = pd.read_csv("Data-mining-I/iris_labels-1.csv")
+data = pd.read_csv("Data-mining-I/iris_data.csv", delimiter=";")
+labels = pd.read_csv("Data-mining-I/iris_labels-1.csv", delimiter=";")
 
-data = pd.read_csv("iris_data.csv")
-labels = pd.read_csv("iris_labels-1.csv")
+# data = pd.read_csv("iris_data.csv", delimiter=";")
+# labels = pd.read_csv("iris_labels-1.csv", delimiter=";")
 
 print("Shape of data: ", data.shape)
 print("Head of data", data.head)
@@ -14,3 +14,6 @@ print("Shape of labels: ", labels.shape)
 print("Head of labels: ", labels.head)
 
 dataframe = pd.merge(data, labels, on = 'id', how = "inner")
+
+print("Shape of labels: ", dataframe.shape)
+print("Head of labels: ", dataframe.head)
